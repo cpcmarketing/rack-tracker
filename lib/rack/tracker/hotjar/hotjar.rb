28f48f6c1b48213566a672d74158d70a2c62305a
molivetti <molivetti@beyondfinance.com>
2023-12-01 15:50:59 -0500

    Updated Hotjar to our correct configuration

--- lib/rack/tracker/hotjar/hotjar.rb
+++ lib/rack/tracker/hotjar/hotjar.rb
@@ -1,2 +1,13 @@
 class Rack::Tracker::Hotjar < Rack::Tracker::Handler
+  # self.position = :body
+  # self.allowed_tracker_options = [:site_id]
+
+  class Event < OpenStruct
+  end
+
+  class Identify < Event
+    def name
+      'identify'
+    end
+  end
 end
