8be6e4adbe6497425a387853ae5186b12b563095
molivetti <molivetti@beyondfinance.com>
2023-12-12 16:35:03 -0500

    Updated to match how we implement GTM in BillDoctor.

--- lib/rack/tracker/google_tag_manager/google_tag_manager.rb
+++ lib/rack/tracker/google_tag_manager/google_tag_manager.rb
@@ -19,10 +19,6 @@ class Rack::Tracker::GoogleTagManager < Rack::Tracker::Handler
     response
   end
 
-  def container
-    options[:container].respond_to?(:call) ? options[:container].call(env) : options[:container]
-  end
-
   def render_head
     Tilt.new( File.join( File.dirname(__FILE__), 'template', 'google_tag_manager_head.erb') ).render(self)
   end
